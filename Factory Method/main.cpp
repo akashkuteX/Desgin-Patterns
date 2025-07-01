818ab6fbcc4d3bc4052b8d78b327d7d32bb07dc9
Akash Kute <kuteakash51@gmail.com>
2025-06-30 20:48:23 -0400

    class-diagram

---
+++ Factory Method/main.cpp
@@ -0,0 +1,36 @@
+/* Class Diagram
+
+**Abstract Factory**
+    --createButton()
+    --createCheckbox()
+
+**ConcreteFactory1 (WindowFactory)**
+    --createButton() -> WindowsButton
+    --createCheckbox() -> WindowsCheckbox
+
+**ConcreteFactory2 (MacFactory)**
+    --createButton() -> MacButton
+    --createCheckbox -> MacCheckbox
+
+
+AbstractProductA (Button)
+AbstractProductB (Checkbox)
+
+ConcreteProductA1 (WindowsButton)
+ConcreteProductB1 (WindowsCheckbox)
+
+ConcreteProductA1 (MacButton)
+ConcreteProductB2 (MacCheckbox)
+
+
+*/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
