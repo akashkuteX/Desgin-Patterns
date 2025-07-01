1cafe4fbb26ac69741b67d4762652b0b88a5bd41
Akash Kute <kuteakash51@gmail.com>
2025-06-30 20:56:02 -0400

    added-code-following-abstract-factory

--- Factory Method/main.cpp
+++ Factory Method/main.cpp
@@ -25,12 +25,118 @@ ConcreteProductB2 (MacCheckbox)
 
 */
 
-
-
-
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+// Abstract Products
+class Button
+{
+public:
+    virtual void paint() = 0;
+};
 
-return 0;
+class Checkbox
+{
+public:
+    virtual void check() = 0;
+};
+
+// Concrete Products - Windows
+class WindowsButton : public Button
+{
+public:
+    void paint() override
+    {
+        std::cout << "Rendering Windows Button\n";
+    }
+};
+
+class WindowsCheckbox : public Checkbox
+{
+public:
+    void check() override
+    {
+        std::cout << "Windows Checkbox Checked\n";
+    }
+};
+
+// Concrete Products - Mac
+class MacButton : public Button
+{
+public:
+    void paint() override
+    {
+        std::cout << "Rendering Mac Button\n";
+    }
+};
+
+class MacCheckbox : public Checkbox
+{
+public:
+    void check() override
+    {
+        std::cout << "Mac Checkbox Checked\n";
+    }
+};
+
+// Abstract Factory
+class GUIFactory
+{
+public:
+    virtual Button *createButton() = 0;
+    virtual Checkbox *createCheckbox() = 0;
+};
+
+// Concrete Factories
+class WindowsFactory : public GUIFactory
+{
+public:
+    Button *createButton() override
+    {
+        return new WindowsButton();
+    }
+    Checkbox *createCheckbox() override
+    {
+        return new WindowsCheckbox();
+    }
+};
+
+class MacFactory : public GUIFactory
+{
+public:
+    Button *createButton() override
+    {
+        return new MacButton();
+    }
+    Checkbox *createCheckbox() override
+    {
+        return new MacCheckbox();
+    }
+};
+
+// Client
+void renderUI(GUIFactory *factory)
+{
+    Button *button = factory->createButton();
+    Checkbox *checkbox = factory->createCheckbox();
+
+    button->paint();
+    checkbox->check();
+
+    delete button;
+    delete checkbox;
+}
+
+int main()
+{
+    GUIFactory *factory;
+
+    factory = new WindowsFactory();
+    renderUI(factory);
+    delete factory;
+
+    factory = new MacFactory();
+    renderUI(factory);
+    delete factory;
+
+    return 0;
 }
